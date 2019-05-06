a63e689a95eff4d233f94e48ed619cceb99fe150
Hendro <awebened@gmail.com>
2019-05-06 08:55:27 +0700

    initial commit

---
+++ Learning_English/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Learning_English
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
